eb0bab82794da7f8b6c350e6a3b6ad536f557b7f
Alexey <alexey.mueller81@gmail.com>
2026-01-12 14:12:29 +0100

    Fixes of 06.02 and 06.03

--- lesson_06/homework_06.02.py
+++ lesson_06/homework_06.02.py
@@ -3,12 +3,12 @@
 Напишіть цикл, який буде вимагати від користувача ввести слово, в якому є літера "h" (враховуються як великі так і маленькі).
 Цикл не повинен завершитися, якщо користувач ввів слово без букви "h". """
 
-line = input("Enter your line: ")
-letter = "h"
-letter_2 = "H"
 
-for char in line:
-    if (char == letter) or (char == letter_2):
-        print("We have a letter h")
-    else:
-        continue
+
+line = input("Enter your line: ")
+
+
+while "h" not in line.lower():
+    line = input("Enter your line: ")
+
+print("We have a letter h")
